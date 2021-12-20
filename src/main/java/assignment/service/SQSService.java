138d37553523cde6f2ac0500ecbeb74579bcb113
s3740819 <s3740819@rmit.edu.vn>
2021-12-20 23:55:31 +0700

    switched to kafka

--- src/main/java/assignment/service/SQSService.java
+++ src/main/java/assignment/service/SQSService.java
@@ -3,23 +3,19 @@ package assignment.service;
 import assignment.entity.User;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.Map;
 
-@Transactional
 @Service
 public class SQSService {
-    @Autowired
-    private QueueMessagingTemplate queueMessagingTemplate;
+    private static final String TOPIC = "user_service";
 
-    @Value("${cloud.aws.end-point.uri}")
-    private String endpoint;
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     public void postUserQueue(User user, String action) {
         Map<String, String> map = new HashMap<>();
@@ -27,14 +23,14 @@ public class SQSService {
         map.put("email", user.getEmail());
         map.put("gender", String.valueOf(user.isGender()));
         JSONObject json = new JSONObject(map);
-        queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(action + "----------" + json.toString()).build());
+        this.kafkaTemplate.send(TOPIC, (action + "----------" + json.toString()));
     }
 
     public void deleteUserQueue(String id) {
         Map<String, String> map = new HashMap<>();
         map.put("id", id);
         JSONObject json = new JSONObject(map);
-        queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload("delete----------" + json.toString()).build());
+        this.kafkaTemplate.send(TOPIC,("delete----------" + json.toString()));
     }
 
 }
