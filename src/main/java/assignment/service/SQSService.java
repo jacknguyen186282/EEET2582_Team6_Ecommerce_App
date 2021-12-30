47147829fb16da85faf11a2e911c672e1a1481d6
s3740819 <s3740819@rmit.edu.vn>
2021-12-31 02:30:22 +0700

    Update SQSService.java

--- src/main/java/assignment/service/SQSService.java
+++ src/main/java/assignment/service/SQSService.java
@@ -3,6 +3,7 @@ package assignment.service;
 import assignment.entity.Product;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import java.util.HashMap;
@@ -14,6 +15,9 @@ public class SQSService {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    ProductService productService;
 
     void postProductQueue(Product product, String action) {
         Map<String, String> map = new HashMap<>();
@@ -29,6 +33,23 @@ public class SQSService {
         map.put("id", id);
         JSONObject json = new JSONObject(map);
         this.kafkaTemplate.send(TOPIC,"delete----------" + json.toString());
+    }
+
+    @KafkaListener(topics = "product_request", groupId = "group_id")
+    public void loadMessageFromProductSQS(String message)  {
+        try {
+            JSONObject obj = new JSONObject(message.split("----------")[1]);
+            String action = message.split("----------")[0];
+
+            Product product = new Product(null, (String) obj.get("name"), (String) obj.get("category"), (String) obj.get("subcategory"), Double.parseDouble((String) obj.get("price")), Boolean.getBoolean((String) obj.get("isnew")), (String) obj.get("image_url"), Integer.parseInt((String) obj.get("stock")), (String) obj.get("description"));
+            
+            if (action.equals("add")) productService.addProduct(product);
+            else if (action.equals("update")) productService.updateProduct(product);
+            else productService.deleteByProductId((String) obj.get("id"));
+        }
+        catch (Exception e){
+            System.out.println("Receive message from SQS Queue: Dummy message");
+        }
     }
 
 }
