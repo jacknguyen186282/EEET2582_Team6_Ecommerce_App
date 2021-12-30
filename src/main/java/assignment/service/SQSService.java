553a2b0b6a382c3485d94d7bfca082978b06a49a
s3740819 <s3740819@rmit.edu.vn>
2021-12-31 02:34:02 +0700

    Update SQSService.java

--- src/main/java/assignment/service/SQSService.java
+++ src/main/java/assignment/service/SQSService.java
@@ -3,6 +3,7 @@ package assignment.service;
 import assignment.entity.User;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,9 @@ public class SQSService {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @Autowired
+    private UserService userService;
+
     public void postUserQueue(User user, String action) {
         Map<String, String> map = new HashMap<>();
         map.put("email", user.getEmail());
@@ -30,6 +34,21 @@ public class SQSService {
         map.put("id", id);
         JSONObject json = new JSONObject(map);
         this.kafkaTemplate.send(TOPIC,("delete----------" + json.toString()));
+    }
+
+    @KafkaListener(topics = "user_request", groupId = "group_id")
+    public void loadMessageFromUserSQS(String message)  {
+        try {
+            JSONObject obj = new JSONObject(message.split("----------")[1]);
+            String action = message.split("----------")[0];
+
+            if (action.equals("add")) userService.addUser(new User((String) obj.get("email")));
+            else if (action.equals("update")) userService.updateUser(new User((String) obj.get("email")));
+            else userService.deleteByUserId((String) obj.get("id"));
+        }
+        catch (Exception e){
+            System.out.println("Receive message from SQS Queue: Dummy message");
+        }
     }
 
 }
